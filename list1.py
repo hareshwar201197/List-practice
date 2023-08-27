b936d964bb9b2daa9fdf588dddff44f938cca348
hari <hareshwarmali20@gmail.com>
2023-08-27 20:59:23 +0530

    sum of list items

---
+++ list1.py
@@ -0,0 +1,18 @@
+
+# Sum of all items in list
+
+def sum_list(item):
+    sum_of_items = 0
+    for value in item:
+        sum_of_items += value
+    return sum_of_items
+
+print(sum_list([-11,21,51,101,-111]))
+
+# lst = [11,21,51,101,111]
+#
+# sum_element = 0
+# for value in lst:
+#     sum_element +=value
+#
+# print(sum_element)
